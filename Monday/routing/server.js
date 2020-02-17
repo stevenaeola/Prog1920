324dc379fb75d5c16ed06ab60347d9147d9ff64d
dcs0spb <s.p.bradley@durham.ac.uk>
2020-02-17 18:03:04 +0000

    session 17/2/2020

--- Monday/routing/server.js
+++ Monday/routing/server.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var fs = require('fs');
+
 var recipes = require("./recipes.json");
+
 
 
 app.get("/", function (request, response){
@@ -22,6 +29,19 @@ app.post("/shout", function (request, response){
     response.send("HELLO TO " +who);
 });
 
+app.post("/new", function (request, response){
+    const title = request.body.title;
+    const url = request.body.url;
+    const newRecipe = {"title": title,
+                       "href": url};
+    recipes.push(newRecipe);
+    const json = JSON.stringify(recipes);
+    fs.writeFile('recipes.json', json, 'utf8', console.log);
+
+    response.send(`Added recipe ${title} ${url}`);
+});
+
+
 app.get("/potatoes", function (request, response){
     const key = request.query.key;
     for(let i = 0; i<recipes.length; i++){
@@ -31,6 +51,19 @@ app.get("/potatoes", function (request, response){
     }
 });
 
+app.get("/search", function (request, response){
+    const keyword = request.query.keyword;
+    console.log(`Searching for ${keyword}`);
+    for(let i = 0; i<recipes.length; i++){
+        console.log("Comparing with " + recipes[i].title);
+        if(recipes[i].title.toLowerCase().includes(keyword.toLowerCase())){
+            console.log("found");
+            response.send(recipes[i].href);
+            console.log("Sent " + recipes[i].href);
+        }
+    }
+
+});
 
 app.listen(8090);
 
