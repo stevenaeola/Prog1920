c48865fda7bba02c73044416af1321beab13f065
stevenaeola <s.p.bradleydurham.ac.uk>
2020-02-27 16:26:41 +0000

    Updates in lecture 21/2/2020

---
+++ Friday/helloevent.js
@@ -0,0 +1,15 @@
+var events = require('events');
+var e = new events.EventEmitter();
+ 
+function say(data){
+  console.log("Hello " + data)
+  e.emit('done')
+}
+ 
+e.on('say', say);
+ 
+e.on('done', function(){
+  console.log('Message done');
+})
+ 
+e.emit('say', 'world');
