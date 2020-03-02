5745e144b0cf3d951cd8244cecb5684040a20987
dcs0spb <s.p.bradley@durham.ac.uk>
2020-03-02 20:16:08 +0000

    Updated HTML. Closes #2

--- Monday/routing/client/index.js
+++ Monday/routing/client/index.js
@@ -12,7 +12,24 @@ form.addEventListener('submit', async function(event){
     let response = await fetch('http://127.0.0.1:8090/search?keyword=' + keyword);
 
     let body = await response.text();
+    let results = JSON.parse(body);
+
+    let resultsDiv = document.getElementById('search_results');
+    
+    results.innerHTML = body;
+
+
+    for(let result of results){
+        let a = document.createElement("a");
+        a.setAttribute('href', result.href);
+        
+        let img = document.createElement("img");
+        img.setAttribute('src', result.thumbnail);
+        img.setAttribute('alt', result.title);
+        a.append(img);
+        resultsDiv.append(a);
+    }
 
     /* amend page with results */
-    alert(body);
+//    alert(body);
 });
